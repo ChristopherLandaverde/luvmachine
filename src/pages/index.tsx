c84d7f266c1feb0b4174b427351ece4a4c894a34
Milan <milan@mdaverde.com>
2023-01-21 13:05:18 -0500

    max-w-sm until responsive
    Signed-off-by: Milan <milan@mdaverde.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,38 +18,44 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${inter.className} bg-secondary pt-36`}>
-        <div className="mb-36">
-          <div className="flex justify-center mb-24">
-            <div className="h-[200px] w-[200px] border-2 border-white-500 rounded-lg text-sm text-white p-8">
-              Logo graphic goes here
+        <div className="max-w-sm mx-auto">
+          <div className="mb-36">
+            <div className="flex justify-center mb-24">
+              <div className="h-[200px] w-[200px] border-2 border-white-500 rounded-lg text-sm text-white p-8">
+                Logo graphic goes here
+              </div>
+            </div>
+            <h1
+              className={`text-6xl font-bold text-center mb-6 ${dancingScript.className}`}
+            >
+              Luv Machine
+            </h1>
+            <h2 className="text-md text-center mb-20">
+              Let Us State Your Message!
+            </h2>
+            <div className="text-center">
+              <button className="text-white bg-primary p-4 rounded-full text-lg w-[200px]">
+                Get started
+              </button>
             </div>
           </div>
-          <h1 className={`text-6xl font-bold text-center mb-6 ${dancingScript.className}`}>
-            Luv Machine
-          </h1>
-          <h2 className="text-md text-center mb-20">
-            Let Us State Your Message!
-          </h2>
-          <div className="text-center">
-            <button className="text-white bg-primary p-4 rounded-full text-lg w-[200px]">Get started</button>
+          <div className="p-4">
+            <label className="block mb-4">
+              <span className="block">To:</span>
+              <input className="block border border-black w-full" type="text" />
+            </label>
+            <label className="block mb-4">
+              <span className="block">From:</span>
+              <input className="block border border-black w-full" type="text" />
+            </label>
+            <label className="block mb-4">
+              <span className="block">Message:</span>
+              <input
+                className="block border border-black h-[150px] w-full"
+                type="text"
+              />
+            </label>
           </div>
-        </div>
-        <div className="p-4">
-          <label className="block mb-4">
-            <span className="block">To:</span>
-            <input className="block border border-black w-full" type="text" />
-          </label>
-          <label className="block mb-4">
-            <span className="block">From:</span>
-            <input className="block border border-black w-full" type="text" />
-          </label>
-          <label className="block mb-4">
-            <span className="block">Message:</span>
-            <input
-              className="block border border-black h-[150px] w-full"
-              type="text"
-            />
-          </label>
         </div>
       </main>
     </>
