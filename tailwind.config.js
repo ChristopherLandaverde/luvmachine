820351d2a97d9051c0bcba49769cbccd7a5f544b
Milan <milan@mdaverde.com>
2023-01-21 13:02:25 -0500

    dancing script font and main page with colors
    Signed-off-by: Milan <milan@mdaverde.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#9747ff",
+        secondary: "#d5b5db",
+      },
+    },
   },
   plugins: [],
 };
